0e5babeeb1974d3696f6a94c093eec16f525cd26
Norbert Schneider <norbert.schneider@ibm.com>
2021-12-12 20:16:53 +0100

    Day 10 - Part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -2,7 +2,9 @@ import Chunk.*
 
 fun main() {
     val lines = readInput("Day10")
-    val syntaxErrorScore = lines.mapNotNull(::check).sumOf {
+    val syntaxCheck = lines.map(::check)
+
+    val syntaxErrorScore = syntaxCheck.mapNotNull { it.first }.sumOf {
         when (it) {
             PARENTHESIS -> 3L
             BRACKETS -> 57
@@ -10,12 +12,28 @@ fun main() {
             ANGLE_BRACKETS -> 25137
         }
     }
+
+    val completionScores = syntaxCheck.filter { it.first == null }
+        .map { (_, expected) ->
+            expected.fold(0L) { sum, chunk ->
+                sum * 5 +
+                        when (chunk) {
+                            PARENTHESIS -> 1
+                            BRACKETS -> 2
+                            CURLY_BRACKETS -> 3
+                            ANGLE_BRACKETS -> 4
+                        }
+            }
+        }.sorted()
+    val medianCompletionScore = completionScores[completionScores.size / 2]
+
     println("Syntax Error Score: $syntaxErrorScore")
+    println("Median Completing Score: $medianCompletionScore")
 }
 
-tailrec fun check(rest: String, expected: ArrayDeque<Chunk> = ArrayDeque()): Chunk? {
+tailrec fun check(rest: String, expected: ArrayDeque<Chunk> = ArrayDeque()): Pair<Chunk?, List<Chunk>> {
     if (rest.isEmpty()) {
-        return null
+        return null to expected
     }
     val current = rest.first()
     val open = Chunk.forOpen(current)
@@ -25,11 +43,12 @@ tailrec fun check(rest: String, expected: ArrayDeque<Chunk> = ArrayDeque()): Chu
     }
     val close = Chunk.forClose(current)
     if (close != null) {
-        val exp = expected.removeFirst()
+        val exp = expected.firstOrNull()
         return if (exp == close) {
+            expected.removeFirst()
             check(rest.drop(1), expected)
         } else {
-            close
+            close to expected
         }
     }
     throw IllegalStateException("Invalid character $current")
